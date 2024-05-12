6a84841dbc5201f9a36b6c1c5cddeec157013b95
hadyanbakti <123660163+hadyanbakti@users.noreply.github.com>
2024-05-12 17:41:36 +0700

    Add files via upload

---
+++ ratingfilm/src/koneksi/connector.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+/**
+ *
+ * @author MSI GF63
+ */
+public class connector {
+    static Connection con;
+    public static Connection connection(){
+        if(con==null){
+    MysqlDataSource data = new MysqlDataSource();
+    data.setDatabaseName("datafilm");
+    data.setUser("root");
+    data.setPassword("");
+    try{
+    con = data.getConnection();
+        System.out.println("koneksi berhasil");
+    }catch(SQLException ex){ 
+        ex.printStackTrace();
+        System.out.println("Koneksi gagal");
+    }
+    }
+        return con;
+    }
+}
